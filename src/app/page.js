762054a364564a78905866362a083d4561f2a054
sergeishay <sergeishay@gmail.com>
2024-07-07 10:27:34 +0300

    hero testing

--- src/app/page.js
+++ src/app/page.js
@@ -35,7 +35,7 @@ export default function Home() {
       </AnimatePresence>
       <Landing />
       <Description />
-      <Projects />
+      {/* <Projects /> */}
       <SlidingImages />
       <Contact />
     </main>
