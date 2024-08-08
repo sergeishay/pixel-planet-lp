9d52273a4d3b05458e74d8c5d5352848553219f7
sergeishay <sergeishay@gmail.com>
2024-08-08 13:50:17 +0300

    tag manager integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import './globals.css';
 import { Montserrat_Alternates } from 'next/font/google';
 import { Rubik } from 'next/font/google';
 import Header from '../components/Header';
+import Head from 'next/head';
+import Script from 'next/script';
 
 const rubik = Rubik({ subsets: ['latin'] });
 const montserrat = Montserrat_Alternates({
@@ -50,10 +52,41 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" >
+    <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href={metadata.icons.icon} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta property="og:description" content={metadata.openGraph.description} />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:site_name" content={metadata.openGraph.siteName} />
+        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+        <meta property="og:locale" content={metadata.openGraph.locale} />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta name="twitter:card" content={metadata.twitter.card} />
+        <meta name="twitter:title" content={metadata.twitter.title} />
+        <meta name="twitter:description" content={metadata.twitter.description} />
+        <meta name="twitter:image" content={metadata.twitter.images[0].url} />
+      </Head>
       <body className={`${rubik.className} ${montserrat.variable}`}>
         <Header />
         {children}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NCZK936H');`
+          }}
+        />
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NCZK936H"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
       </body>
     </html>
   );
