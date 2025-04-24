6c2d906da1ba62412e5edc7086fa9388558646bb
Sergei <sergeishay@gmail.com>
2025-04-24 08:50:17 +0300

    loader update

--- src/app/page.js
+++ src/app/page.js
@@ -13,28 +13,28 @@ import Clients from '../components/Clients/Clients';
 import Whatsapp from '../components/Whatsapp/Whatsapp';
 export default function Home() {
 
-  // const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-  // useEffect( () => {
-  //   (
-  //     async () => {
-  //         const LocomotiveScroll = (await import('locomotive-scroll')).default
-  //         const locomotiveScroll = new LocomotiveScroll();
+  useEffect( () => {
+    (
+      async () => {
+          const LocomotiveScroll = (await import('locomotive-scroll')).default
+          const locomotiveScroll = new LocomotiveScroll();
 
-  //         setTimeout( () => {
-  //           setIsLoading(false);
-  //           document.body.style.cursor = 'default'
-  //           window.scrollTo(0,0);
-  //         }, 2000)
-  //     }
-  //   )()
-  // }, [])
+          setTimeout( () => {
+            setIsLoading(false);
+            document.body.style.cursor = 'default'
+            window.scrollTo(0,0);
+          }, 2000)
+      }
+    )()
+  }, [])
 
   return (
     <main className={styles.main}>
-      {/* <AnimatePresence mode='wait'>
+      <AnimatePresence mode='wait'>
         {isLoading && <Preloader />}
-      </AnimatePresence> */}
+      </AnimatePresence>
       <Landing />
       <Clients />
       <Description />
